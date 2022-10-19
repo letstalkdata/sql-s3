156f4d8d107fd02fda7e248233e9db4f7e67ac38
Sanjeev <letstalkdata@outlook.com>
2022-10-19 06:25:39 -0700

    added scripts

---
+++ polybase/demo.sh
@@ -0,0 +1,11 @@
+#Start up our environment with docker compose. This can take a second for SQL Server to come online.
+cd ./polybase
+docker-compose up --build 
+
+
+##Jump over to demo.sql and run the code there on your SQL Server instance.
+
+
+##Remove the images we built and also the volumes we created. 
+docker-compose down --rmi local --volumes
+rm -rf ./certs
